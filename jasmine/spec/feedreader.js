2a26a65e1592a92cce0068d4ee7184701a497070
Reka-Csima <rcsima7@gmail.com>
2018-07-05 19:31:18 +0200

    Adds spec for defining feed name

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,17 +26,25 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('URL is defined', function() {
+           allFeeds.forEach(function(feed) {
+             expect(feed.url)toBeDefined();
+             expect(feed.url.length).not.toBe(0);
+           });
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('Name is defined', function() {
+           allFeeds.forEach(function(feed) {
+             expect(feed.name)toBeDefined();
+             expect(feed.name.length).not.toBe(0);
     });
 
 
