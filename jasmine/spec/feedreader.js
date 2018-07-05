598c656c99e89f9dfcd5a16ac3f2a64d00e22395
Reka-Csima <rcsima7@gmail.com>
2018-07-05 20:50:50 +0200

    Adds test suite named New Feed Selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -75,18 +75,48 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+      /* TODO: Write a test that ensures when the loadFeed
+       * function is called and completes its work, there is at least
+       * a single .entry element within the .feed container.
+       * Remember, loadFeed() is asynchronous so this test will require
+       * the use of Jasmine's beforeEach and asynchronous done() function.
+       */
+       beforeEach(function (done) {
+        loadFeed(0, function() {
+          done();
+        });
+       });
+
+       it('there is at least one entry when loadFeed completed', function() {
+         expect(document.querySelector(".feed").getElementsByClassName("entry-link")).toBeDefined();
+       });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+      /* TODO: Write a test that ensures when a new feed is loaded
+       * by the loadFeed function that the content actually changes.
+       * Remember, loadFeed() is asynchronous.
+       */
+
+       beforeEach(function(done) {
+         loadFeed(0, function() {
+           var initialFeed = document.querySelector(".feed").innerHTML;
+         });
+
+         loadFeed(1, function() {
+           var otherFeed = document.querySelector(".feed").innerHTML;
+         });
+
+         done();
+       });
+
+       it('content actually changes', function() {
+         expect(initialFeed.).not.toBe(otherFeed);
+       });
+    });
+
 }());
